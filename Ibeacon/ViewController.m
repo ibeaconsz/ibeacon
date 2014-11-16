958bee1452552eac7a15291ec2c4d78f3baf63dc
jay.mao <mjie46@126.com>
2014-11-16 11:39:32 +0800

    kjk

--- Ibeacon/ViewController.m
+++ Ibeacon/ViewController.m
@@ -9,9 +9,11 @@
 #import "ViewController.h"
 #import "APLDefaults.h"
 @import CoreLocation;
-@interface ViewController ()<CLLocationManagerDelegate>
+@interface ViewController ()<CLLocationManagerDelegate,UIAlertViewDelegate>
 {
     BOOL isGetA;
+    BOOL stopGetAData;
+    BOOL starNavFlag;
 }
 @property NSMutableDictionary *beacons;
 @property CLLocationManager *locationManager;
@@ -33,7 +35,33 @@
     _distanceLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 420, 300, 30)];
     _distanceLabel.textColor = [UIColor blueColor];
     [self.view addSubview:_distanceLabel];
+    
+    UIButton *starNav = [UIButton buttonWithType:UIButtonTypeCustom];
+    starNav.frame = CGRectMake(220, 450, 80, 30);
+    [starNav setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [starNav setTitle:@"开始导航" forState:UIControlStateNormal];
+    [starNav addTarget:self action:@selector(onStarNavButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:starNav];
+    
+    [_escalatorAddress addTarget:self action:@selector(onESCbuttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    starNavFlag = NO;
     [self initIbeacon];
+}
+
+- (IBAction)onStarNavButtonAction:(id)sender
+{
+    starNavFlag = YES;
+}
+
+- (IBAction)onESCbuttonAction:(id)sender
+{
+    self.escalatorAddress.hidden = NO;
+    //寒星画线
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"寒星画线" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
+    stopGetAData = NO;
+    [_mylocation setBackgroundColor:[UIColor greenColor]];
+    
 }
 
 - (void)initIbeacon
@@ -96,7 +124,7 @@
     
 //    NSLog(@"beacons : %@",self.beacons)÷
     
-    if (self.beacons.count) {
+    if (self.beacons.count && starNavFlag) {
         // Display the UUID, major, minor and accuracy for each beacon.
         NSNumber *sectionKey = [self.beacons allKeys][0];
         CLBeacon *beacon = self.beacons[sectionKey][0];
@@ -111,16 +139,27 @@
         
 //        NSLog(@"%.2f",(beacon.accuracy*1000)*(beacon.accuracy)*1000);
         NSLog(@"xxx:%d",yy);
-        if (yy > 0) {
+        if (yy > 0 && !stopGetAData) {
             CGRect frame = _mylocation.frame;
 //            frame.origin.x = _wcAddress.center.x+80;
             if (isGetA) {
-                frame.origin.y = _wcAddress.frame.origin.y-[temp floatValue]*100 < CGRectGetMinY(_escalatorAddress.frame) ? CGRectGetMinY(_escalatorAddress.frame) : _wcAddress.frame.origin.y+[temp floatValue]*100;
+                frame.origin.y = _wcAddress.frame.origin.y-[temp floatValue]*100 < CGRectGetMinY(_escalatorAddress.frame) ? CGRectGetMinY(_escalatorAddress.frame) : _wcAddress.frame.origin.y-[temp floatValue]*100;
+                if (frame.origin.y == CGRectGetMinY(_escalatorAddress.frame)) {
+                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"到达扶梯,演示结束" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alertView show];
+                    stopGetAData = YES;
+                    [_mylocation setBackgroundColor:[UIColor purpleColor]];
+                }
             }
             else{
                 frame.origin.y = _wcAddress.frame.origin.y+[temp floatValue]*100 < CGRectGetMinY(_escalatorAddress.frame) ? CGRectGetMinY(_escalatorAddress.frame) : _wcAddress.frame.origin.y+[temp floatValue]*100;
-                if ([temp floatValue] <= 0.2) {
+                if (frame.origin.y <= _wcAddress.frame.origin.y + 50 && frame.origin.y >= _wcAddress.frame.origin.y - 50) {
                     isGetA = YES;
+                    frame.origin.y = _wcAddress.frame.origin.y;
+                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"到达洗手间" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alertView show];
+                    stopGetAData = YES;
+                    [_mylocation setBackgroundColor:[UIColor redColor]];
                 }
             }
             
@@ -142,6 +181,13 @@
        didFailWithError:(NSError *)error
 {
     
+}
+
+#pragma mark UIAlertViewDelegate
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+//    stopGetAData = NO;
+//    [_mylocation setBackgroundColor:[UIColor greenColor]];
 }
 
 
